ce31482c9d8cc50a7ee353d8d51c818827066907
Paul Pladziewicz <ppladziewicz@gmail.com>
2024-09-05 09:38:37 -0400

    base controllers for new features and fw-medium update to form labels

---
+++ src/main/java/com/paulpladziewicz/fremontmi/controllers/NeighborServiceController.java
@@ -0,0 +1,15 @@
+package com.paulpladziewicz.fremontmi.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class NeighborServiceController {
+
+    @GetMapping("/neighbor-service/{id}")
+    public String viewNeighborService(@PathVariable String id, Model model) {
+        return "neighborservices/neighborservices-page";
+    }
+}
