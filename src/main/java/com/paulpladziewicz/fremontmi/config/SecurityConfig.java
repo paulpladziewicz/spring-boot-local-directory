bd5711e8a13dddb39646cf9bf59d9382d1bf9ab8
Paul Pladziewicz <ppladziewicz@gmail.com>
2024-09-25 06:39:03 -0400

    removed unnecessary config

--- src/main/java/com/paulpladziewicz/fremontmi/config/SecurityConfig.java
+++ src/main/java/com/paulpladziewicz/fremontmi/config/SecurityConfig.java
@@ -48,7 +48,7 @@ public class SecurityConfig {
         http.authorizeHttpRequests((authorize) -> authorize
                 .requestMatchers(HttpMethod.POST, "/register", "/contact", "/subscribe", "/api/stripe/**").permitAll()
                 .requestMatchers(HttpMethod.GET, "/api/stripe/**").permitAll()
-                .requestMatchers("/groups/","/groups/**","/events","/events/**", "/register", "/forgot-password", "/reset-password", "/forgot-username", "/css/**", "/privacy-policy", "/terms-of-service", "/js/**", "/images/**", "/favicon.ico", "/error", "/login", "/login?error", "/login?error=*", "/login?logout", "/businesses", "/businesses/**", "/neighbor-services", "/neighbor-services/**", "/autocomplete/tags", "/confirm", "/tagging-guidelines").permitAll()
+                .requestMatchers("/groups/","/groups/**","/events","/events/**", "/register", "/forgot-password", "/reset-password", "/forgot-username", "/css/**", "/privacy-policy", "/terms-of-service", "/js/**", "/images/**", "/favicon.ico", "/error", "/login", "/login?error", "/login?error=*", "/login?logout", "/businesses", "/businesses/**", "/neighbor-services", "/neighbor-services/**", "/confirm", "/tagging-guidelines").permitAll()
                 .anyRequest().authenticated()
         );
         http.formLogin(formLogin -> formLogin
