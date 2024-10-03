adfc3547c4c986db01c2be17b739f0fb182237f4
Paul Pladziewicz <ppladziewicz@gmail.com>
2024-10-02 21:26:01 -0400

    using an application property for mapbox token

--- src/main/java/com/paulpladziewicz/fremontmi/controllers/ArticleController.java
+++ src/main/java/com/paulpladziewicz/fremontmi/controllers/ArticleController.java
@@ -1,11 +1,15 @@
 package com.paulpladziewicz.fremontmi.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ArticleController {
+
+    @Value("${mapbox.secret}")
+    private String mapboxSecret;
 
     @GetMapping("/articles")
     public String articles(Model model) {
@@ -14,6 +18,7 @@ public class ArticleController {
 
     @GetMapping("/articles/parks")
     public String parks(Model model) {
+        model.addAttribute("mapboxSecret", mapboxSecret);
         return "articles/parks";
     }
 }
