71dd600d20a71f7b69875c2282f1dadc0c9641a9
Paul Pladziewicz <ppladziewicz@gmail.com>
2024-10-08 16:23:44 -0400

    unused import

--- src/main/java/com/paulpladziewicz/fremontmi/services/UserService.java
+++ src/main/java/com/paulpladziewicz/fremontmi/services/UserService.java
@@ -8,7 +8,6 @@ import com.paulpladziewicz.fremontmi.repositories.UserRepository;
 import jakarta.validation.ValidationException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.dao.DataAccessException;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
