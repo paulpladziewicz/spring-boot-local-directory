7a48d668ac7fea9beda16d2fc502e541bbfacc22
Paul Pladziewicz <ppladziewicz@gmail.com>
2024-10-11 06:48:39 -0400

    progress

--- src/main/java/com/paulpladziewicz/fremontmi/controllers/ArticleController.java
+++ src/main/java/com/paulpladziewicz/fremontmi/controllers/ArticleController.java
@@ -1,5 +1,6 @@
 package com.paulpladziewicz.fremontmi.controllers;
 
+import com.paulpladziewicz.fremontmi.services.EmailService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -17,18 +18,16 @@ public class ArticleController {
     @Value("${mapbox.secret}")
     private String mapboxSecret;
 
+    private final EmailService emailService;
+
+    public ArticleController(EmailService emailService) {
+        this.emailService = emailService;
+    }
+
     @GetMapping("/create/article")
     public String create(Model model) {
         return "articles/create-article";
     }
-
-    @PostMapping("/uploadFile")
-    public ResponseEntity<?> uploadFile(@RequestParam("file") MultipartFile file) {
-        // Implement file handling logic
-        String imageUrl = "/uploads/" + file.getOriginalFilename();
-        return ResponseEntity.ok(Map.of("success", 1, "file", Map.of("url", imageUrl)));
-    }
-
 
     @GetMapping("/articles")
     public String articles(Model model) {
@@ -44,5 +43,15 @@ public class ArticleController {
     @GetMapping("/articles/prepare-to-vote")
     public String registerToVote(Model model) {
         return "articles/prepare-to-vote";
+    }
+
+    @GetMapping("/articles/coming-soon-taqueria-de-gallo")
+    public String ComingSoonTaqueriaDeGallo(Model model) {
+        return "articles/coming-soon-taqueria-de-gallo";
+    }
+
+    @PostMapping("/taqueria")
+    public void taqueriaContactForm() {
+        // call email service directly
     }
 }
