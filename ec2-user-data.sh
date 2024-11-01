b6fb37dd5ea9a228f4b011ff57ecf1d2547dc308
Paul Pladziewicz <ppladziewicz@gmail.com>
2024-11-01 12:58:50 -0400

    incrementing major version

--- ec2-user-data.sh
+++ ec2-user-data.sh
@@ -8,11 +8,11 @@ sudo yum install -y nginx
 sudo yum install -y java-21-amazon-corretto-headless
 
 # Download the required files
-aws s3 cp s3://westmichigansoftware/deployments/fremontmi-1.1.11.jar /home/ec2-user/fremontmi-1.1110.jar
+aws s3 cp s3://westmichigansoftware/deployments/fremontmi-2.0.0.jar /home/ec2-user/fremontmi-2.0.0jar
 sudo aws s3 cp s3://westmichigansoftware/deployments/nginx.conf /etc/nginx/nginx.conf
 
 # Start the application
-sudo bash -c 'nohup java -jar /home/ec2-user/fremontmi-1.1.11.jar > /var/log/app.log 2>&1 &'
+sudo bash -c 'nohup java -jar /home/ec2-user/fremontmi-2.0.0.jar > /var/log/app.log 2>&1 &'
 
 # Start and enable NGINX
 sudo systemctl start nginx
