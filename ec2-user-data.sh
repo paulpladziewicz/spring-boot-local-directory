f6c00adfff0335debb909795efeb1f376e3969c9
Paul Pladziewicz <ppladziewicz@gmail.com>
2024-11-06 21:03:36 -0500

    updating ec2 user data script to use a variable

--- ec2-user-data.sh
+++ ec2-user-data.sh
@@ -3,6 +3,9 @@
 # Log user data output for debugging
 exec > >(tee /var/log/user-data.log | logger -t user-data) 2>&1
 
+# Define the JAR version variable
+JAR_VERSION="2.0.9"
+
 # Update the instance
 sudo yum update -y
 
@@ -11,7 +14,7 @@ sudo yum install -y nginx
 sudo yum install -y java-21-amazon-corretto-headless
 
 # Download the required files
-aws s3 cp s3://westmichigansoftware/deployments/fremontmi-2.0.9.jar /home/ec2-user/fremontmi-2.0.9.jar
+aws s3 cp s3://westmichigansoftware/deployments/fremontmi-${JAR_VERSION}.jar /home/ec2-user/fremontmi-${JAR_VERSION}.jar
 sudo aws s3 cp s3://westmichigansoftware/deployments/nginx.conf /etc/nginx/nginx.conf
 
 # Check if port 8080 is already in use, and kill the process if it is
@@ -20,7 +23,7 @@ if sudo lsof -i :8080; then
 fi
 
 # Start the application
-sudo bash -c 'nohup java -jar /home/ec2-user/fremontmi-2.0.9.jar > /var/log/app.log 2>&1 &'
+sudo bash -c "nohup java -jar /home/ec2-user/fremontmi-${JAR_VERSION}.jar > /var/log/app.log 2>&1 &"
 
 # Start and enable NGINX
 sudo systemctl start nginx
