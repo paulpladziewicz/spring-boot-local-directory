4c94809efe6b9bb606b5ff7fc38386a8847ed521
Paul Pladziewicz <ppladziewicz@gmail.com>
2024-11-06 21:06:44 -0500

    incrementing version

--- deployment-script.sh
+++ deployment-script.sh
@@ -14,12 +14,12 @@ POM_FILE="pom.xml"
 sed -i.bak 's/<version>\(.*\)-SNAPSHOT<\/version>/<version>\1<\/version>/' "$POM_FILE"
 rm "$POM_FILE.bak"
 
-# Capture the pre-incremented version without -SNAPSHOT
+# Capture the pre-incremented version
 CURRENT_VERSION=$(awk '/<artifactId>fremontmi<\/artifactId>/ {getline; print}' "$POM_FILE" | sed -E 's/.*<version>([^<]+)<\/version>.*/\1/')
 
-# Update the EC2 user data script with the pre-incremented version
+# Explicitly check and update EC2 user data with CURRENT_VERSION
 USER_DATA_SCRIPT="ec2-user-data.sh"
-sed -i.bak "s/fremontmi-[0-9]*\.[0-9]*\.[0-9]*\.jar/fremontmi-${CURRENT_VERSION}.jar/" "$USER_DATA_SCRIPT"
+sed -i.bak "s/^JAR_VERSION=\".*\"/JAR_VERSION=\"${CURRENT_VERSION}\"/" "$USER_DATA_SCRIPT"
 rm "$USER_DATA_SCRIPT.bak"
 
 # Build the package
@@ -28,33 +28,18 @@ mvn clean package -Pprod
 # Put application dev application properties back
 cp "$DEV_PROPS_FILE" "$APP_PROPS_FILE"
 
-# Update the package version and add snapshot back
-POM_FILE="pom.xml"
-IFS='.' read -r MAJOR MINOR PATCH <<< "${CURRENT_VERSION%%-SNAPSHOT}"
+# Increment version and re-add SNAPSHOT
+IFS='.' read -r MAJOR MINOR PATCH <<< "$CURRENT_VERSION"
 MAJOR_INCREMENT=0
 MINOR_INCREMENT=0
 PATCH_INCREMENT=1
 MAJOR=$((MAJOR + MAJOR_INCREMENT))
 MINOR=$((MINOR + MINOR_INCREMENT))
 PATCH=$((PATCH + PATCH_INCREMENT))
-if (( MAJOR_INCREMENT > 0 )); then
-  MINOR=0
-  PATCH=0
-elif (( MINOR_INCREMENT > 0 )); then
-  PATCH=0
-fi
 NEW_VERSION="${MAJOR}.${MINOR}.${PATCH}-SNAPSHOT"
 sed -i.bak "/<artifactId>fremontmi<\/artifactId>/,/<\/version>/ s/<version>.*<\/version>/<version>${NEW_VERSION}<\/version>/" "$POM_FILE"
 rm "$POM_FILE.bak"
 
-# Update ec2-user-data
-USER_DATA_SCRIPT="ec2-user-data.sh"
-CURRENT_JAR_VERSION=$(awk '/<artifactId>fremontmi<\/artifactId>/ {getline; print}' "$POM_FILE" | sed -E 's/.*<version>([^<]+)<\/version>.*/\1/')
-sed -i.bak "s/fremontmi-[0-9]*\.[0-9]*\.[0-9]*\.jar/fremontmi-${CURRENT_JAR_VERSION}.jar/" "$USER_DATA_SCRIPT"
-rm "$USER_DATA_SCRIPT.bak"
-
 # Git commit
-#git add "$POM_FILE"
-#git commit -m "Incrementing to $NEW_VERSION"
-
-
+#git add "$POM_FILE" "$USER_DATA_SCRIPT"
+#git commit -m "Incrementing to $NEW_VERSION and updating EC2 user data script"
