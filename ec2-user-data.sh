b0e0b655fdd3d83ddf9a96e90fdc1d1417bf5bf7
Paul Pladziewicz <ppladziewicz@gmail.com>
2024-11-06 21:42:33 -0500

    updating where the jar goes

--- ec2-user-data.sh
+++ ec2-user-data.sh
@@ -14,8 +14,8 @@ sudo yum install -y nginx
 sudo yum install -y java-21-amazon-corretto-headless
 
 # Download the required files
-aws s3 cp s3://westmichigansoftware/deployments/fremontmi-${JAR_VERSION}.jar /home/ec2-user/fremontmi-${JAR_VERSION}.jar
-sudo aws s3 cp s3://westmichigansoftware/deployments/nginx.conf /etc/nginx/nginx.conf
+aws s3 cp s3://fremontmi/deployment/fremontmi-${JAR_VERSION}.jar /home/ec2-user/fremontmi-${JAR_VERSION}.jar
+sudo aws s3 cp s3://fremontmi/deployment/nginx.conf /etc/nginx/nginx.conf
 
 # Check if port 8080 is already in use, and kill the process if it is
 if sudo lsof -i :8080; then
