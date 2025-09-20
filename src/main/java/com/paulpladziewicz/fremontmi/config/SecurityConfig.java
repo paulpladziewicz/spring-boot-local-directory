4340667cdad5312686ecd443b4faeb49c321bd7f
paulpladziewicz <ppladziewicz@gmail.com>
2025-09-20 06:16:52 -0400

    creating separate dev security config

--- src/main/java/com/paulpladziewicz/fremontmi/config/SecurityConfig.java
+++ src/main/java/com/paulpladziewicz/fremontmi/config/SecurityConfig.java
@@ -1,7 +1,5 @@
 package com.paulpladziewicz.fremontmi.config;
 
-import com.paulpladziewicz.fremontmi.repositories.UserRepository;
-import com.paulpladziewicz.fremontmi.services.CustomUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -10,16 +8,14 @@ import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.Arrays;
+import com.paulpladziewicz.fremontmi.repositories.UserRepository;
+import com.paulpladziewicz.fremontmi.services.CustomUserDetailsService;
 
 @Configuration
 @EnableWebSecurity
@@ -34,40 +30,36 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((authorize) -> authorize
-                .requestMatchers(HttpMethod.POST, "/api/vectors/search","/api/upload","/register", "/contact", "/subscribe", "/api/stripe/**", "/article-contact", "/taqueria", "/contact/neighbor-services-profile").permitAll()
-                .requestMatchers("/search","/about", "/api/subscribe", "/api/events","/overview/**","/articles/**","/groups/","/groups/**","/events","/events/**", "/register", "/forgot-password", "/reset-password", "/forgot-username", "/css/**", "/privacy-policy", "/terms-of-service", "/js/**", "/images/**", "/favicon.ico", "/error", "/login", "/login?error", "/login?error=*", "/login?logout", "/businesses", "/businesses/**", "/neighbor-services", "/neighbor-services/**", "/confirm", "/tagging-guidelines", "/create/neighbor-services-profile/overview", "/create/business/overview", "/", "/robots.txt", "/sitemap.xml", "/health", "/group/**", "/event/**", "/business/**", "/neighbor-services-profile/**").permitAll()
-                .anyRequest().authenticated()
-        );
+                .requestMatchers(HttpMethod.POST, "/api/vectors/search", "/api/upload", "/register", "/contact",
+                        "/subscribe", "/api/stripe/**", "/article-contact", "/taqueria",
+                        "/contact/neighbor-services-profile")
+                .permitAll()
+                .requestMatchers("/search", "/about", "/api/subscribe", "/api/events", "/overview/**", "/articles/**",
+                        "/groups/", "/groups/**", "/events", "/events/**", "/register", "/forgot-password",
+                        "/reset-password", "/forgot-username", "/css/**", "/privacy-policy", "/terms-of-service",
+                        "/js/**", "/images/**", "/favicon.ico", "/error", "/login", "/login?error", "/login?error=*",
+                        "/login?logout", "/businesses", "/businesses/**", "/neighbor-services", "/neighbor-services/**",
+                        "/confirm", "/tagging-guidelines", "/create/neighbor-services-profile/overview",
+                        "/create/business/overview", "/", "/robots.txt", "/sitemap.xml", "/health", "/group/**",
+                        "/event/**", "/business/**", "/neighbor-services-profile/**")
+                .permitAll()
+                .anyRequest().authenticated());
         http.formLogin(formLogin -> formLogin
                 .loginPage("/login")
                 .permitAll()
                 .failureHandler(customAuthenticationFailureHandler())
-                .successHandler(customSavedRequestAwareAuthenticationSuccessHandler())
-        );
+                .successHandler(customSavedRequestAwareAuthenticationSuccessHandler()));
         http.requestCache(requestCacheCustomizer -> requestCacheCustomizer
-                .requestCache(new CustomRequestCache())
-        );
+                .requestCache(new CustomRequestCache()));
         http.logout(logout -> logout
                 .logoutSuccessUrl("/")
                 .invalidateHttpSession(true)
                 .deleteCookies("JSESSIONID")
-                .permitAll()
-        );
+                .permitAll());
         http.csrf((csrf) -> csrf
-                .ignoringRequestMatchers("/login")
-        );
+                .ignoringRequestMatchers("/login"));
         return http.build();
     }
-
-//    @Bean
-//    UrlBasedCorsConfigurationSource corsConfigurationSource() {
-//        CorsConfiguration configuration = new CorsConfiguration();
-//        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
-//        configuration.setAllowedMethods(Arrays.asList("GET","POST", "OPTIONS"));
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        source.registerCorsConfiguration("/**", configuration);
-//        return source;
-//    }
 
     @Bean
     public CustomSavedRequestAwareAuthenticationSuccessHandler customSavedRequestAwareAuthenticationSuccessHandler() {
@@ -80,7 +72,8 @@ public class SecurityConfig {
     }
 
     @Bean
-    public AuthenticationManager authenticationManager(UserDetailsService userDetailsService, PasswordEncoder passwordEncoder) {
+    public AuthenticationManager authenticationManager(UserDetailsService userDetailsService,
+            PasswordEncoder passwordEncoder) {
         DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
         authenticationProvider.setUserDetailsService(userDetailsService);
         authenticationProvider.setPasswordEncoder(passwordEncoder);
